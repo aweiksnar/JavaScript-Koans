1adaa15f934e093ebb63dcf7f0dc39824102531c
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-11-03 21:01:43 -0600

    all the koans

--- topics/about_scope.js
+++ topics/about_scope.js
@@ -17,5 +17,5 @@ test("variables declared inside of a function", function() {
     })();
 
     equal("outer", outerVariable, 'is outerVariable defined in this scope?');
-    equal(undefined, typeof(innerVariable), 'is innerVariable defined in this scope?');
+    equal("undefined", typeof(innerVariable), 'is innerVariable defined in this scope?');
 });
