4ab7622bec1e182870ce9e7f1af60815ff5c4861
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-11-03 19:18:46 -0600

    30 koans

--- topics/about_strings.js
+++ topics/about_strings.js
@@ -4,23 +4,23 @@ module("About Strings (topics/about_strings.js)");
 test("delimiters", function() {
     var singleQuotedString = 'apple';
     var doubleQuotedString = "apple";
-    equal(__, singleQuotedString === doubleQuotedString, 'are the two strings equal?');
+    equal(true, singleQuotedString === doubleQuotedString, 'are the two strings equal?');
 });
 
 test("concatenation", function() {
     var fruit = "apple";
     var dish = "pie";
-    equal(__, fruit + " " + dish, 'what is the value of fruit + " " + dish?');
+    equal("apple pie", fruit + " " + dish, 'what is the value of fruit + " " + dish?');
 });
 
 test("character Type", function() {
     var characterType = typeof("Amory".charAt(1)); // typeof will be explained in about reflection
-    equal(__, characterType, 'Javascript has no character type');
+    equal("string", characterType, 'Javascript has no character type');
 });
 
 test("escape character", function() {
     var stringWithAnEscapedCharacter  = "\u0041pple";
-    equal(__, stringWithAnEscapedCharacter, 'what  is the value of stringWithAnEscapedCharacter?');
+    equal("Apple", stringWithAnEscapedCharacter, 'what  is the value of stringWithAnEscapedCharacter?');
 });
 
 test("string.length", function() {
